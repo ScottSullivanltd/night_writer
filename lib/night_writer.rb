f53be63718182a40dff3e042355d03770d886a16
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-04 19:59:56 -0700

    Feat: update to read given file and print number of characters in that file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,7 @@
-puts "Created '#{ARGV[0]}' containing 256 characters"
+read_file = File.open(ARGV[0], "r")
+
+file_text = read_file.read
+
+read_file.close
+
+puts "Created '#{ARGV[0]}' containing #{file_text.length} characters"
