cc169920dc92d9e82a31c51ddf808820b4a661d2
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-06 00:17:24 -0700

    Feat: complete characters library

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,12 +4,11 @@ require 'pry'
 
 message_file = File.open(ARGV[0], 'r')
 
-file_text = message_file.read
-message = file_text.chomp
+file_text = message_file.read.chomp
 
 message_file.close
 
-puts "Created '#{ARGV[0]}' containing #{file_text.length - 1} characters"
+puts "Created '#{ARGV[0]}' containing #{file_text.length} characters"
 
 braille_file = File.open(ARGV[1], 'w')
 
