06d64cd4f77d462076c1847a34456f61612f26b2
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-06 13:14:20 -0700

    Feat: add add method to get first row of braille

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,18 @@ require 'pry'
 require './lib/text_to_braille'
 
 class Translator
-  attr_reader :message
+  attr_reader :message, :braille_text, :english_text
 
   def initialize(message)
     @message = message.chars
+    @english_text = TextToBraille.new
+    @braille_text = Hash.new { |h, k| h[k] = [] }
   end
+
+  def braille_row_one
+    @message.each do |char|
+      @braille_text[:one] << @english_text.characters[char][0]
+    end
+    @braille_text[:one].flatten!
+  end  
 end
