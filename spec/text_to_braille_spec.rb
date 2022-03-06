9614c35f969b65caa072bd6b53b3b6af241efc6c
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-05 22:29:53 -0700

    Test: dictionary is a hash of braille values

--- spec/text_to_braille_spec.rb
+++ spec/text_to_braille_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe TextToBraille do
 
     expect(english_text).to be_an_instance_of(TextToBraille)
   end
+
+  it "creates a dictionary of characters as a hash" do
+    characters = {
+      ' ' => [['..'], ['..'], ['..']],
+      'a' => [['0.'], ['..'], ['..']],
+      'b' => [['0.'], ['0.'], ['..']]
+    }
+    expect(characters.class).to eq(Hash)
+  end
+
+  it "has english character as a key and corresponding braille as value" do
+    characters = {
+      ' ' => [['..'], ['..'], ['..']],
+      'a' => [['0.'], ['..'], ['..']],
+      'b' => [['0.'], ['0.'], ['..']]
+    }
+    expect(characters['a']).to eq([['0.'], ['..'], ['..']])
+  end
 end
