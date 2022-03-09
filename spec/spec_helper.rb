bb0331c09de105c1d4ca7aeca5ce322be8ca28a1
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-08 18:56:33 -0700

    Updated spec_helper - for real this time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
-require './spec/night_writer_spec'
+require './text_to_braille_spec'
 require './spec/translator_spec'
-require './spec/night_reader_spec'
+require './spec/braille_to_text_spec'
 require './spec/interpretor_spec'
 
 # run following from root directory
